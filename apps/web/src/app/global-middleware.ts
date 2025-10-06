118c4148c4d046686102e6f754a920bab8a2f537
Yujong Lee <yujonglee.dev@gmail.com>
2025-10-06 20:41:19 +0900

    init api and web app

---
+++ apps/web/src/app/global-middleware.ts
@@ -0,0 +1,8 @@
+import * as Sentry from "@sentry/tanstackstart-react";
+import { createMiddleware, registerGlobalMiddleware } from "@tanstack/react-start";
+
+registerGlobalMiddleware({
+  middleware: [
+    createMiddleware().server(Sentry.sentryGlobalServerMiddlewareHandler()),
+  ],
+});
