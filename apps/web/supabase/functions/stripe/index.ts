f0218beb3186fc9adc40cc96d505d6bd963e2f86
Yujong Lee <yujonglee.dev@gmail.com>
2025-10-25 15:04:59 +0900

    fix supabase function deploy

--- apps/web/supabase/functions/stripe/index.ts
+++ apps/web/supabase/functions/stripe/index.ts
@@ -1,12 +1,18 @@
+import { createClient } from "@supabase/supabase-js";
 import Stripe from "stripe";
 
 const stripe = new Stripe(Deno.env.get("STRIPE_API_KEY") as string, {
-  apiVersion: "2025-02-24.acacia",
+  apiVersion: "2025-09-30.clover",
 });
 
 const cryptoProvider = Stripe.createSubtleCryptoProvider();
 
 Deno.serve(async (request) => {
+  const _supabaseClient = createClient(
+    Deno.env.get("SUPABASE_URL") ?? "",
+    Deno.env.get("SUPABASE_SECRET_KEY") ?? "",
+  );
+
   const signature = request.headers.get("Stripe-Signature");
 
   if (!signature) {
