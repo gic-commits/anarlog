8a7a0b6f09ad8c82d64d8513f8ad3d4b4fdff049
Yujong Lee <yujonglee.dev@gmail.com>
2025-10-27 13:51:00 +0900

    fix compile error

--- crates/llm/src/task.rs
+++ crates/llm/src/task.rs
@@ -14,11 +14,11 @@ pub async fn generate_title(
         messages: vec![
             hypr_llama::LlamaMessage {
                 role: "system".into(),
-                content: render(Template::CreateTitleSystem, &ctx).unwrap(),
+                content: render(Template::TitleSystem, &ctx).unwrap(),
             },
             hypr_llama::LlamaMessage {
                 role: "user".into(),
-                content: render(Template::CreateTitleUser, &ctx).unwrap(),
+                content: render(Template::TitleUser, &ctx).unwrap(),
             },
         ],
         max_tokens: Some(30),
@@ -37,75 +37,5 @@ pub async fn generate_title(
         .collect::<Vec<_>>();
     let text = items.join("");
 
-    Ok(text)
-}
-
-pub async fn generate_tags(
-    provider: &ModelManager,
-    ctx: serde_json::Map<String, serde_json::Value>,
-) -> Result<Vec<String>, crate::Error> {
-    let model = provider.get_model().await?;
-
-    let stream = model.generate_stream(hypr_llama::LlamaRequest {
-        messages: vec![
-            hypr_llama::LlamaMessage {
-                role: "system".into(),
-                content: render(Template::SuggestTagsSystem, &ctx).unwrap(),
-            },
-            hypr_llama::LlamaMessage {
-                role: "user".into(),
-                content: render(Template::SuggestTagsUser, &ctx).unwrap(),
-            },
-        ],
-        max_tokens: Some(30),
-        grammar: Some(Grammar::Tags.build()),
-        ..Default::default()
-    })?;
-
-    let items = stream
-        .collect::<Vec<_>>()
-        .await
-        .into_iter()
-        .filter_map(|r| match r {
-            hypr_llama::Response::TextDelta(content) => Some(content.clone()),
-            _ => None,
-        })
-        .collect::<Vec<_>>();
-    let text = items.join("");
-    let tags = serde_json::from_str::<Vec<String>>(&text).unwrap_or_default();
-    Ok(tags)
-}
-
-pub async fn postprocess_transcript(
-    provider: &ModelManager,
-    ctx: serde_json::Map<String, serde_json::Value>,
-) -> Result<String, crate::Error> {
-    let model = provider.get_model().await?;
-
-    let stream = model.generate_stream(hypr_llama::LlamaRequest {
-        messages: vec![
-            hypr_llama::LlamaMessage {
-                role: "system".into(),
-                content: render(Template::PostprocessTranscriptSystem, &ctx).unwrap(),
-            },
-            hypr_llama::LlamaMessage {
-                role: "user".into(),
-                content: render(Template::PostprocessTranscriptUser, &ctx).unwrap(),
-            },
-        ],
-        max_tokens: Some(100),
-        ..Default::default()
-    })?;
-
-    let items = stream
-        .collect::<Vec<_>>()
-        .await
-        .into_iter()
-        .filter_map(|r| match r {
-            hypr_llama::Response::TextDelta(content) => Some(content.clone()),
-            _ => None,
-        })
-        .collect::<Vec<_>>();
-    let text = items.join("");
     Ok(text)
 }
