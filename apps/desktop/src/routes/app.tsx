7bc7f0e06db4b799c65766734454d9f957416b48
Yujong Lee <yujonglee.dev@gmail.com>
2025-10-30 20:30:18 +0900

    install quit handler based on notif setting

--- apps/desktop/src/routes/app.tsx
+++ apps/desktop/src/routes/app.tsx
@@ -1,6 +1,9 @@
 import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { useEffect, useMemo } from "react";
 
+import { commands as detectCommands } from "@hypr/plugin-detect";
 import { ListenerProvider } from "../contexts/listener";
+import * as main from "../store/tinybase/main";
 
 export const Route = createFileRoute("/app")({
   component: Component,
@@ -12,9 +15,26 @@ export const Route = createFileRoute("/app")({
 function Component() {
   const { listenerStore } = Route.useLoaderData();
 
+  useQuitHandler();
+
   return (
     <ListenerProvider store={listenerStore}>
       <Outlet />
     </ListenerProvider>
   );
+}
+
+function useQuitHandler() {
+  const notification_detect = main.UI.useValue("notification_detect", main.STORE_ID);
+  const notification_event = main.UI.useValue("notification_event", main.STORE_ID);
+
+  const active = useMemo(() => notification_detect || notification_event, [notification_detect, notification_event]);
+
+  useEffect(() => {
+    if (active) {
+      detectCommands.setQuitHandler();
+    } else {
+      detectCommands.resetQuitHandler();
+    }
+  }, [active]);
 }
