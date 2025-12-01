590f1c28208507f0e9bc8f9fd43778bceb207282
Yujong Lee <yujonglee.dev@gmail.com>
2025-12-01 09:48:57 +0900

    fix env issue in apps/api

--- apps/api/src/env.ts
+++ apps/api/src/env.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 
 export const env = createEnv({
   server: {
-    PORT: z.number().default(8787),
+    PORT: z.coerce.number().default(8787),
     NODE_ENV: z
       .enum(["development", "test", "production"])
       .default("development"),
