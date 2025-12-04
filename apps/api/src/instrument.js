02312bf2d585e73a35bdb705f267df14e63230b3
Yujong Lee <yujonglee.dev@gmail.com>
2025-12-04 11:26:59 +0900

    fix sentry dsn stuffs

--- apps/api/src/instrument.js
+++ apps/api/src/instrument.js
@@ -1,6 +1,7 @@
 import * as Sentry from "@sentry/bun";
 
 Sentry.init({
-  dsn: "https://a4abe058104d9e2142abe78f702e3de9@o4506190168522752.ingest.us.sentry.io/4508570874937344",
+  dsn: Bun.env.SENTRY_DSN,
+  release: `hyprnote-api@${Bun.env.APP_REVISION ?? "local"}`,
   sampleRate: 1.0,
 });
