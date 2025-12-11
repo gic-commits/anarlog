ec6b26214e756e8094a9bb2036952b839817a601
Yujong Lee <yujonglee.dev@gmail.com>
2025-12-11 19:48:48 +0900

    fix app version info for posthog

--- plugins/analytics/build.rs
+++ plugins/analytics/build.rs
@@ -3,7 +3,10 @@ const COMMANDS: &[&str] = &["event", "set_properties", "set_disabled", "is_disab
 fn main() {
     println!("cargo:rerun-if-env-changed=POSTHOG_API_KEY");
 
-    let gitcl = vergen_gix::GixBuilder::all_git().unwrap();
+    let gitcl = vergen_gix::GixBuilder::default()
+        .describe(true, true, Some("desktop_v*"))
+        .build()
+        .unwrap();
     vergen_gix::Emitter::default()
         .add_instructions(&gitcl)
         .unwrap()
