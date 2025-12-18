3934c8212a4f0526c41a52fcd0c21045cb57d9db
Yujong Lee <yujonglee.dev@gmail.com>
2025-12-18 22:11:37 +0900

    update check interval

--- plugins/updater2/src/lib.rs
+++ plugins/updater2/src/lib.rs
@@ -38,8 +38,8 @@ pub fn init<R: tauri::Runtime>() -> tauri::plugin::TauriPlugin<R> {
             let handle = app.clone();
             tauri::async_runtime::spawn(async move {
                 loop {
-                    tokio::time::sleep(std::time::Duration::from_secs(5)).await;
                     job::check_and_download_update(&handle).await;
+                    tokio::time::sleep(std::time::Duration::from_secs(60 * 60)).await;
                 }
             });
             Ok(())
