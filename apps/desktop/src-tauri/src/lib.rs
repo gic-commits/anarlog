dccbd85d52011a88feb59ba11186adf34a36b286
Yujong Lee <yujonglee.dev@gmail.com>
2026-01-15 18:23:32 +0900

    add bedrock plugin

--- apps/desktop/src-tauri/src/lib.rs
+++ apps/desktop/src-tauri/src/lib.rs
@@ -91,6 +91,7 @@ pub async fn main() {
         .plugin(tauri_plugin_opener2::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_analytics::init())
+        .plugin(tauri_plugin_bedrock::init())
         .plugin(tauri_plugin_importer::init())
         .plugin(tauri_plugin_apple_calendar::init())
         .plugin(tauri_plugin_apple_contact::init())
---
+++ plugins/bedrock/src/lib.rs
@@ -0,0 +1,59 @@
+use tauri::Manager;
+
+mod commands;
+mod error;
+mod ext;
+
+pub use error::{Error, Result};
+pub use ext::*;
+
+pub type ManagedState = aws_sdk_bedrock::Client;
+
+const PLUGIN_NAME: &str = "bedrock";
+
+fn make_specta_builder<R: tauri::Runtime>() -> tauri_specta::Builder<R> {
+    tauri_specta::Builder::<R>::new()
+        .plugin_name(PLUGIN_NAME)
+        .commands(tauri_specta::collect_commands![
+            commands::list_foundation_models::<tauri::Wry>,
+        ])
+        .error_handling(tauri_specta::ErrorHandlingMode::Result)
+}
+
+pub fn init<R: tauri::Runtime>() -> tauri::plugin::TauriPlugin<R> {
+    let specta_builder = make_specta_builder();
+
+    tauri::plugin::Builder::new(PLUGIN_NAME)
+        .invoke_handler(specta_builder.invoke_handler())
+        .setup(|app, _api| {
+            let config = tauri::async_runtime::block_on(aws_config::load_defaults(
+                aws_config::BehaviorVersion::latest(),
+            ));
+            let client = aws_sdk_bedrock::Client::new(&config);
+            assert!(app.manage(client));
+            Ok(())
+        })
+        .build()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn export_types() {
+        const OUTPUT_FILE: &str = "./js/bindings.gen.ts";
+
+        make_specta_builder::<tauri::Wry>()
+            .export(
+                specta_typescript::Typescript::default()
+                    .formatter(specta_typescript::formatter::prettier)
+                    .bigint(specta_typescript::BigIntExportBehavior::Number),
+                OUTPUT_FILE,
+            )
+            .unwrap();
+
+        let content = std::fs::read_to_string(OUTPUT_FILE).unwrap();
+        std::fs::write(OUTPUT_FILE, format!("// @ts-nocheck\n{content}")).unwrap();
+    }
+}
