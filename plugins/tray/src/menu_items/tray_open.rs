f2fa8071eeed9de0b6858d3e5e5d24082826e7d6
Yujong Lee <yujonglee.dev@gmail.com>
2026-02-07 12:04:28 +0900

    cleanup tray version name

--- plugins/tray/src/menu_items/tray_open.rs
+++ plugins/tray/src/menu_items/tray_open.rs
@@ -7,30 +7,12 @@ use super::MenuItemHandler;
 
 pub struct TrayOpen;
 
-impl TrayOpen {
-    fn get_channel(app_name: &str) -> &'static str {
-        match app_name {
-            "Hyprnote" => "stable",
-            "Hyprnote Nightly" => "nightly",
-            "Hyprnote Staging" => "staging",
-            _ => "dev",
-        }
-    }
-}
-
 impl MenuItemHandler for TrayOpen {
     const ID: &'static str = "hypr_tray_open";
 
     fn build(app: &AppHandle<tauri::Wry>) -> Result<MenuItemKind<tauri::Wry>> {
-        let app_version = app.package_info().version.to_string();
-        let app_name = &app.package_info().name;
-        let channel = Self::get_channel(app_name);
-
-        let title = if channel == "stable" {
-            format!("Open Hyprnote  {}", app_version)
-        } else {
-            format!("Open Hyprnote  {} ({})", app_version, channel)
-        };
+        let package = app.package_info();
+        let title = format!("Open {}  {}", package.name, package.version);
 
         let item = MenuItem::with_id(app, Self::ID, &title, true, None::<&str>)?;
         Ok(MenuItemKind::MenuItem(item))
